22e1ccc7c8274a25582f03a98e4db1e968b3ed4f
ANMOL PAL <91240707+ANMOLPALIIT-ISM@users.noreply.github.com>
2023-08-15 19:21:35 +0530

    Update auth.js

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -23,7 +23,7 @@ exports.auth = async (req, res, next) => {
 
         
         try{
-            const decode =  jwt.verify(token,'anmolpal');
+            const decode =  jwt.verify(token,process.env.JWT_SECRET);
             console.log(decode);
             req.user = decode;
         }
